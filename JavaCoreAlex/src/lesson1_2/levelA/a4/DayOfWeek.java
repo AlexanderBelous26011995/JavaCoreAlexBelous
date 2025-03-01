912a99ee63885fed7e36915e6f980561ed709050
JavaCoreAlex <alex.belous.1995@mail.ru>
2025-03-01 18:38:47 +0300

    Создал enum класс с именем DayOfWeek и перечислил в нем дни недели.

---
+++ JavaCoreAlex/src/lesson1_2/levelA/a4/DayOfWeek.java
@@ -0,0 +1,15 @@
+package lesson1_2.levelA.a4;
+/*
+Создайте enum класс с именем DayOfWeek для дней недели.
+ */
+public class DayOfWeek {
+    enum Day {
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY
+    }
+}
