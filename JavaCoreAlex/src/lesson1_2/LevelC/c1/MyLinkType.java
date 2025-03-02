5c884e5801b4b888abbeb551e9eedfce82070448
JavaCoreAlex <alex.belous.1995@mail.ru>
2025-03-02 12:48:31 +0300

    В классе Run создал переменную ссылочного типа MyLinkType.

---
+++ JavaCoreAlex/src/lesson1_2/LevelC/c1/MyLinkType.java
@@ -0,0 +1,4 @@
+package lesson1_2.LevelC.c1;
+
+public class MyLinkType {
+}
---
+++ JavaCoreAlex/src/lesson1_2/LevelC/c1/Run.java
@@ -0,0 +1,11 @@
+package lesson1_2.LevelC.c1;
+
+/*
+В классе Run создайте переменную ссылочного типа MyLinkType.
+ */
+public class Run {
+    public static void main(String[] args) {
+        MyLinkType myLink = new MyLinkType();
+        System.out.println(myLink);
+    }
+}
