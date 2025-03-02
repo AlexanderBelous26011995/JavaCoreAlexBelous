acd327bcd7d343dbb0e26796daca8f0f539e2f6e
JavaCoreAlex <alex.belous.1995@mail.ru>
2025-03-02 11:10:52 +0300

    методе main класса Run создал переменную а типа int со значением 1 и переменную b типа int со значением 2.Вывел в консоль результат математической операции a/b.

---
+++ JavaCoreAlex/src/lesson1_2/levelB/b2/Run.java
@@ -0,0 +1,16 @@
+package lesson1_2.levelB.b2;
+
+/*
+В методе main класса Run создайте переменную а типа int со значением 1 и переменную b типа int со значением 2.
+Выведите в консоль результат математической операции a/b.
+Сверьте результат из консоли с калькулятором, он должен быть одинаковым!
+ */
+public class Run {
+    public static void main(String[] args) {
+        double a = 1;
+        double b = 2;
+        double quotient = a / b;
+        System.out.println(quotient);
+
+    }
+}
