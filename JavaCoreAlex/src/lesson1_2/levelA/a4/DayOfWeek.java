094e6866d196dd6a10845960af8a2cc5dccd775f
JavaCoreAlex <alex.belous.1995@mail.ru>
2025-03-02 10:23:05 +0300

    Рефакторинг кода

--- JavaCoreAlex/src/lesson1_2/levelA/a4/DayOfWeek.java
+++ JavaCoreAlex/src/lesson1_2/levelA/a4/DayOfWeek.java
@@ -2,14 +2,12 @@ package lesson1_2.levelA.a4;
 /*
 Создайте enum класс с именем DayOfWeek для дней недели.
  */
-public class DayOfWeek {
-    enum Day {
-        MONDAY,
-        TUESDAY,
-        WEDNESDAY,
-        THURSDAY,
-        FRIDAY,
-        SATURDAY,
-        SUNDAY
-    }
+public enum DayOfWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
 }
